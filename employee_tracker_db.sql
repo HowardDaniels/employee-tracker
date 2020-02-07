1c3c993544dedf2c862efa29de861a1fe2d08c0f
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-07 08:45:39 -0500

    added role table

--- employee_tracker_db.sql
+++ employee_tracker_db.sql
@@ -17,3 +17,12 @@ salary DECIMAL,
 department_id INT,
 PRIMARY KEY (id)
 );
+
+CREATE TABLE employee(
+* **id** - INT PRIMARY KEY
+  * **first_name** - VARCHAR(30) to hold employee first name
+  * **last_name** - VARCHAR(30) to hold employee last name
+  * **role_id** - INT to hold reference to role employee has
+  * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
+
+);
