019ee97cf89d1737e7d5f535435685e22f2415f2
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-07 08:48:06 -0500

    added employee table

--- employee_tracker_db.sql
+++ employee_tracker_db.sql
@@ -19,10 +19,10 @@ PRIMARY KEY (id)
 );
 
 CREATE TABLE employee(
-* **id** - INT PRIMARY KEY
-  * **first_name** - VARCHAR(30) to hold employee first name
-  * **last_name** - VARCHAR(30) to hold employee last name
-  * **role_id** - INT to hold reference to role employee has
-  * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
-
+id INT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT,
+PRIMARY KEY (id)
 );
