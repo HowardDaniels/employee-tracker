f7e32afdd8df68f2a21013775bf88e92bdcc5daf
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-12 18:37:03 -0500

    added 11 employees

--- seed.sql
+++ seed.sql
@@ -3,3 +3,14 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Steve
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Anne", "Richardson", 1, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Elizabeth", "Scott", 1, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Diego", "Sanchez", 2, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Rhonda", "Stuart", 2, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Louisa", "Li", 2, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Andrew", "Carter", 2, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Karen", "Williams", 3, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Sven", "Schmidt", 3, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Courtney", "Leroy", 3, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Arthur", "Cho", 3, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Pamela", "Willis", 4, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Donna", "Hernandez", 4, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Henry", "Matthews", 4, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Joao", "dos Santos", 4, 4);
