dafadd437c710e27d36aa018995bbb32b549f4d3
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-11 23:40:14 -0500

    added five employees to seed.sql

---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Joe", "Smith", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Steven", "Peterson", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Anne", "Richardson", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Elizabeth", "Scott", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Diego", "Sanchez", 2, 2);
