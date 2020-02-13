96a314e6e0066345c133f05a9f4351ee06d6dcdf
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-12 20:30:43 -0500

    added departments and roles

--- seed.sql
+++ seed.sql
@@ -1,3 +1,13 @@
+INSERT INTO department (name) VALUES ("Accounting");
+INSERT INTO department (name) VALUES ("HR");
+INSERT INTO department (name) VALUES ("IT");
+INSERT INTO department (name) VALUES ("Sales");
+ 
+INSERT INTO role (title, salary, department) VALUES ("Accountant", 75000, "Accounting");
+INSERT INTO role (title, salary, department) VALUES ("Recruiter", 60000, "HR");
+INSERT INTO role (title, salary, department) VALUES ("Developer", 80000, "IT");
+INSERT INTO role (title, salary, department) VALUES ("Sales Agent", 80000, "Sales");
+
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Joe", "Smith", 1, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Steven", "Peterson", 1, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Anne", "Richardson", 1, 1);
