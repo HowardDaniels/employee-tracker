1562165b464d1f241d8df3b033b8e52b09154e7f
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-17 16:37:31 -0500

    fixed role column names and changed the department_id to a number

--- seed.sql
+++ seed.sql
@@ -3,10 +3,10 @@ INSERT INTO department (name) VALUES ("HR");
 INSERT INTO department (name) VALUES ("IT");
 INSERT INTO department (name) VALUES ("Sales");
  
-INSERT INTO role (title, salary, department) VALUES ("Accountant", 75000, "Accounting");
-INSERT INTO role (title, salary, department) VALUES ("Recruiter", 60000, "HR");
-INSERT INTO role (title, salary, department) VALUES ("Developer", 80000, "IT");
-INSERT INTO role (title, salary, department) VALUES ("Sales Agent", 80000, "Sales");
+INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 75000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Recruiter", 60000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Developer", 80000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Sales Agent", 80000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Joe", "Smith", 1, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Steven", "Peterson", 1, 1);
